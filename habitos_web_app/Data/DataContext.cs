59bb09317c7e980db6ab8a909e277820ed3feaea
Santiago Martinez <102550080+SantiagoMGS@users.noreply.github.com>
2024-04-09 20:17:46 -0500

    Conf conexion base de datos

---
+++ habitos_web_app/Data/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using habitos_app.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace habitos_app.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> User { get; set; }
+        //public DbSet<AdminUser> AdminUser { get; set; }
+        //public DbSet<RegularUser> RegularUser { get; set; }
+        //public DbSet<HealthProfessional> HealthProfessional { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        
+
+            // Si necesitas configuraciones específicas para las entidades derivadas, hazlo aquí
+        }
+
+    }
+
+
+
+
+}
--- habitos_web_app/Program.cs
+++ habitos_web_app/Program.cs
@@ -1,9 +1,16 @@
-var builder = WebApplication.CreateBuilder(args);
+using habitos_app.Web.Data;
+using Microsoft.EntityFrameworkCore;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+//Data context (database connection)
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnection")));
+
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
