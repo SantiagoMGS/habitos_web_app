da44ffba4e6f790fa56180727eb5aaf765f5b2a4
sebaslzs <sebastianlopezsanches46@gmail.com>
2024-04-12 19:34:23 -0500

    Agregadas clases y controlador para medicamentos

---
+++ habitos_web_app/Controllers/MedicationsController.cs
@@ -0,0 +1,42 @@
+﻿using habitos_app.Web.Data;
+using habitos_app.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace habitos_web_app.Controllers
+{
+    public class MedicationsController : Controller
+    {
+        private readonly DataContext _context;
+
+        public MedicationsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Medication> medications = await _context.Medication.ToListAsync();
+            return View(medications);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Medication medication)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(medication);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(medication);
+        }
+    }
+}
---
+++ habitos_web_app/Models/Unit.cs
@@ -0,0 +1,16 @@
+﻿namespace habitos_app.Web.Models
+{
+	using System.ComponentModel.DataAnnotations;
+
+	public class Unit
+	{
+		public int Id { get; set; }
+
+		[Required(ErrorMessage = "El nombre de la unidad es obligatorio.")]
+		public string Name { get; set; }
+
+		public string Description { get; set; }
+
+		// Otras propiedades relacionadas con la unidad, si es necesario.
+	}
+}
