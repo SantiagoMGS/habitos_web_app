ff3939615324b13e9c27c98901b46d26a664587b
Karenm99 <karenm9902@gmail.com>
2024-04-14 14:56:13 -0500

    Crear tipo de habito

--- habitos_web_app/Controllers/HabitTypesController.cs
+++ habitos_web_app/Controllers/HabitTypesController.cs
@@ -1,5 +1,6 @@
 ﻿using habitos_app.Web.Data;
 using habitos_app.Web.Models;
+using habitos_web_app.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -19,6 +20,32 @@ namespace habitos_web_app.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _context.HabitType.ToListAsync());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(HabitTypeDto dto)
+        {
+
+            if (ModelState.IsValid)
+            {
+                HabitType habitType = new HabitType
+                {
+                    Description = dto.Description,
+                };
+
+                await _context.HabitType.AddAsync(habitType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(dto);
         }
     }
 }
