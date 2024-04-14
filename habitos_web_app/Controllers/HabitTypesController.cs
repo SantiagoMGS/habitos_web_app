0ea192b0bc9c905330a18b59f7e052bc20461c4a
Karenm99 <karenm9902@gmail.com>
2024-04-14 14:52:42 -0500

    Listar tipos de habito

---
+++ habitos_web_app/Controllers/HabitTypesController.cs
@@ -0,0 +1,24 @@
+﻿using habitos_app.Web.Data;
+using habitos_app.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace habitos_web_app.Controllers
+{
+    public class HabitTypesController : Controller
+    {
+        private readonly DataContext _context;
+
+        public HabitTypesController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.HabitType.ToListAsync());
+        }
+    }
+}
