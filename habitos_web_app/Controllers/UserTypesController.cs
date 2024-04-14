a18bbb865bb4851583d1ed8ae04ad6f68726441e
Karenm99 <karenm9902@gmail.com>
2024-04-14 13:02:38 -0500

    Edit UserType
    Funcionalidad editar tipo usuario

--- habitos_web_app/Controllers/UserTypesController.cs
+++ habitos_web_app/Controllers/UserTypesController.cs
@@ -46,5 +46,47 @@ namespace habitos_web_app.Controllers
 
             return View(dto);
         }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Edit([FromRoute] int id)
+        {
+            // Obtiene el usuario a editar y la lista de tipos de usuario
+            UserType userType = await _context.UserType.FindAsync(id);
+            // Si el usuario no existe, retorna un error 404 y no revienta la aplicación
+            if (userType == null)
+            {
+                return NotFound();
+            }
+
+            UserTypeDto userTypeEditDto = new UserTypeDto
+            {
+                Description = userType.Description,
+            };
+
+            return View(userTypeEditDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(UserTypeDto dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(dto);
+            }
+
+            UserType userType = await _context.UserType.FirstOrDefaultAsync(u => u.Id == dto.Id);
+            if (userType == null)
+            {
+                return NotFound();
+            }
+
+            userType.Description = dto.Description;
+
+            _context.Update(userType);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
