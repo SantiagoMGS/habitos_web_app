2b0b80a112285cdb4506db5f52f25c9d7bc9f280
Santiago Martinez <102550080+SantiagoMGS@users.noreply.github.com>
2024-04-13 20:32:55 -0500

    Update User and create UserType

---
+++ habitos_web_app/Models/UserType.cs
@@ -0,0 +1,14 @@
+﻿namespace habitos_app.Web.Models;
+using System.ComponentModel.DataAnnotations;
+
+public class UserType
+{
+    [Key] // Indica que este campo es la clave primaria
+    public int Id { get; set; }
+
+    [Required]
+    public string Description { get; set; }
+
+    // Lista de usuarios para establecer la relación uno a muchos
+    // public List<User> Users { get; set; } = new List<User>();
+}
